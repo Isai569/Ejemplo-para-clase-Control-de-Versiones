201226be47413554806f61a80bb12076521a5820
TheDarkKrown <111591503+TheDarkKrown@users.noreply.github.com>
2022-09-16 14:38:02 -0400

    Update Program.cs

--- ConsoleApp24/Program.cs
+++ ConsoleApp24/Program.cs
@@ -10,14 +10,15 @@ namespace Primer_Caracter_Apellido_Ultimo_Caracter_Nombre
     {
         static void Main(string[] args)
         {
-            // modificacion del programa para ahora el programa puede mostrar la cantidad de caracteres del nombre
-            string Nombre, Apellidos, tamaño;
+            // modificion del programa para ahora el programa puede mostrar la cantidad de caracteres del nombre y los apellidos
+            string Nombre, Apellidos;
             Console.Write("Escriba su nombre: ");
             Nombre = Console.ReadLine();
             Console.Write("Escriba sus apellidos: ");
             Apellidos = Console.ReadLine()
             Console.WriteLine(Nombre.Last() + " " + Apellidos.First());
             Console.WriteLine("Su nombre contiene " + Nombre.Length + " Caracteres");   
+            Console.WriteLine("Su nombre contiene " + Apellidos.Length + " Caracteres");   
            
             Console.ReadKey();
         }
